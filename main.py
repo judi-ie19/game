d07d783ad05ad9b9c3a9ee5c0f3754da0b2b00ff
jud-ie19 <owuorakinyijudith@gmail.com>
2022-04-16 19:00:51 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("welcome to python quiz game")
+
+playing = input("Do you want to play?")
+
+if playing != "yes":
+    quit()
+
+print("okay! lets play:)")  
+
+answer= input("what does IDE stand for? ")
+
+if answer == "Integrated Development Environment":
+    print('correct!')
+    
+else:print("Incorrect!")
+
+
